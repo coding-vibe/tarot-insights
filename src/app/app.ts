8a64be3c95bd5106dc93ea83e68756121cd48145
Mariia <54949862+coding-vibe@users.noreply.github.com>
2026-01-24 22:41:40 +0200

    add tarot card (#3)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TarotCardComponent } from './tarot-card/tarot-card/tarot-card';
+import { tarotData } from './tarot-card/tarot-data';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TarotCardComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('tarot-insights');
+  cards = tarotData;
 }
